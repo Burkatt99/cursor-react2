713b415760dc5431a6514433252b1f16729d6493
Таня Бура <tanya.bura.99@gmail.com>
2020-04-04 22:27:07 +0300

    Updates

---
+++ precache-manifest.963982cdef03fb377003d751531bdc78.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a8643e4cbfbb09a3339a9cc837d17e",
+    "url": "/cursor-react2/index.html"
+  },
+  {
+    "revision": "10555db54ff88f529d63",
+    "url": "/cursor-react2/static/css/main.0ed87138.chunk.css"
+  },
+  {
+    "revision": "ab63eda7dcebbd63fdbe",
+    "url": "/cursor-react2/static/js/2.ae45b39b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-react2/static/js/2.ae45b39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10555db54ff88f529d63",
+    "url": "/cursor-react2/static/js/main.c306a765.chunk.js"
+  },
+  {
+    "revision": "e702cd1f3ed4396761dc",
+    "url": "/cursor-react2/static/js/runtime-main.a019d9f0.js"
+  },
+  {
+    "revision": "b94d21f4e34cee22064671d2cd88f556",
+    "url": "/cursor-react2/static/media/loupe.b94d21f4.png"
+  },
+  {
+    "revision": "055b64e73b010f6952737f27cd401177",
+    "url": "/cursor-react2/static/media/male.055b64e7.png"
+  },
+  {
+    "revision": "9e63954cd1ec2056a5e40f515f91fe84",
+    "url": "/cursor-react2/static/media/phone-call.9e63954c.png"
+  }
+]);
